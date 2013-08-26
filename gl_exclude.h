3feb44a192f8b35625daf4c2fc74615d55883c0f
Mason James <mtj@kohaaloha.com>
2013-08-26 20:25:11 +1200

    add travis buildbot

--- gl_exclude.h
+++
@@ -1,6 +0,0 @@
-// OpenGL Extension Exclusions - may be modified before building.
-//
-// Generated for NVIDIA Corporation, GeForce 8600 GT/PCI/SSE2/3DNOW!
-// OpenGL v3.3.0, using FreeGLUT v20600
-//
-// Exclusions omitted for distribution build.
